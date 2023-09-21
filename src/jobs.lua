5002fb8678d79e02574054b7a1ba82a8a2c56b49
skitsanos <info@skitsanos.com>
2023-09-21 10:07:23 +0300

    chore: core functionality

---
+++ src/jobs.lua
@@ -0,0 +1,67 @@
+local uuid = require('uuid')
+local jobs = {}
+
+function jobs.new(space_name)
+    local self = {
+        space = box.space[space_name]
+    }
+
+    if not self.space then
+        -- Create the space if it doesn't exist
+        self.space = box.schema.space.create(space_name,
+                {
+                    if_not_exists = true,
+                    format = {
+                        { name = 'job_id', type = 'string' },
+                        { name = 'status', type = 'string' },
+                        { name = 'payload', type = 'map' }
+                    }
+                })
+
+        --self.space:create_index('primary', { if_not_exists = true, sequence = true })
+        self.space:create_index('primary', {
+            type = 'hash',
+            parts = { 'job_id' },
+            if_not_exists = true
+        })
+    end
+
+    -- Internal function to generate a unique job ID
+    local function generate_job_id()
+        return uuid.str()
+    end
+
+    function self.create_job(payload)
+        local job_id = generate_job_id()
+        self.space:insert { job_id, 'pending', payload }
+        return job_id
+    end
+
+    function self.list_all_jobs(limit, offset)
+        limit = limit or 50  -- Default limit is 50
+        offset = offset or 0 -- Default offset is 0
+        local result_set = self.space:select(nil, { limit = limit, offset = offset })
+        local total = self.space:count()
+        return total, result_set
+    end
+
+    function self.fetch_job()
+        local job = self.space.index.primary:select('pending')[1]
+        if job then
+            self.space:update(job[1], { { '=', 2, 'in-progress' } })
+        end
+        return job
+    end
+
+    function self.get_job_by_id(job_id)
+        return self.space:get(job_id)
+    end
+
+    function self.delete_job_by_id(job_id)
+        self.space:delete(job_id)
+    end
+
+    return self
+end
+
+return jobs
---
+++ src/server.lua
@@ -0,0 +1,52 @@
+package.path = '/app/?.lua;' .. package.path
+
+box.cfg({
+    listen = '0.0.0.0:3301',
+    --replication = { '0.0.0.0:3302', '0.0.0.0:3303' },
+})
+
+local jobs_module = require('jobs')
+local jobs_manager = jobs_module.new('jobs_space')
+
+local httpd = require('http.server')
+local json = require('json')
+local server = httpd.new('0.0.0.0', 3000)
+
+server:route({ path = '/version', method = 'GET' }, function()
+    return { status = 200, body = json.encode({
+        version = '1.0.0beta'
+    }) }
+end)
+
+server:route({ path = '/jobs', method = 'POST' }, function(req)
+    local payload = req:json()
+    local job_id = jobs_manager.create_job(payload)
+    return { status = 201, body = json.encode({ job_id = job_id }) }
+end)
+
+server:route({ path = '/jobs', method = 'GET' }, function(req)
+    local limit = tonumber(req:query_param('limit')) or 50
+    local offset = tonumber(req:query_param('offset')) or 0
+    local total, jobs_data = jobs_manager.list_all_jobs(limit, offset)
+    local response = {
+        total = total,
+        limit = limit,
+        offset = offset,
+        data = jobs_data
+    }
+    return { status = 200, body = json.encode(response) }
+end)
+
+server:route({ path = '/jobs/:id', method = 'GET' }, function(req)
+    local job_id = req:stash('id')
+    local job = jobs_manager.get_job_by_id(job_id)
+    return { status = 200, body = json.encode(job) }
+end)
+
+server:route({ path = '/jobs/:id', method = 'DELETE' }, function(req)
+    local job_id = req:stash('id')
+    jobs_manager.delete_job_by_id(job_id)
+    return { status = 204 }
+end)
+
+server:start()
